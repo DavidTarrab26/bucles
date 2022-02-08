72d69e66be2d944672b31c62db64791ac8842f51
Davidtarrab <dudutarrab@gmail.com>
2022-02-08 02:45:29 -0300

    problemas en el 9

--- Ejercicio4/index.js
+++ Ejercicio4/index.js
@@ -1,5 +1,6 @@
 let arr = [4,5,734,43,45];
 
+//Asignamos variables con numeros aleatorios
 let n1 = parseInt(Math.random()*100)
 let n2 = parseInt(Math.random()*100)
 let n3 = parseInt(Math.random()*100)
@@ -10,7 +11,8 @@ let n7 = parseInt(Math.random()*100)
 let n8 = parseInt(Math.random()*100)
 let n9 = parseInt(Math.random()*100)
 let n10 = parseInt(Math.random()*100)
-//Pusheamos numeros al array
 
+//Pusheamos numeros al array
 arr.push(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
+
 console.log(arr )
---
+++ Ejercicio6/index.js
@@ -0,0 +1,7 @@
+let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
+
+//el array.length es igual a 14, por eso se le pone el -1, 
+//para q arranque en 13 y muestre directo el de la ultima posicion 
+for (let i = mySampleArray.length-1; i >= 0; i--) {
+    console.log(mySampleArray[i]);
+}
