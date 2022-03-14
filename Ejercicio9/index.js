06cc6519d49592ab602121e3a66945613aa30ab3
Davidtarrab <dudutarrab@gmail.com>
2022-03-13 23:02:59 -0300

    terminado

--- Ejercicio9/index.js
+++ Ejercicio9/index.js
@@ -1,4 +1,13 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsancursus"
-let counts = {};
-// your code here
-console.log(counts);
+let contadorLetras = {}
+
+for (let i = 0; i < par.length; i++) {
+    if (contadorLetras[par[i]]==undefined){
+        contadorLetras[par[i]] = 1
+    } else {
+        contadorLetras[par[i]]++
+    }
+
+}
+
+console.log(contadorLetras);
